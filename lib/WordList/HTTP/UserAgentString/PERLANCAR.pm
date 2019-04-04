6f42952e63dbfe3eb2070195fde794c1b0afc157
perlancar (on pc-office) <perlancar@gmail.com>
2019-04-04 18:20:01 +0700

    Initial commit

---
+++ lib/WordList/HTTP/UserAgentString/PERLANCAR.pm
@@ -0,0 +1,20 @@
+package WordList::HTTP::UserAgentString::PERLANCAR;
+
+# DATE
+# VERSION
+
+use WordList;
+our @ISA = qw(WordList);
+
+# STATS
+
+1;
+# ABSTRACT: A selection of some HTTP User-Agent strings
+
+=head1 DESCRIPTION
+
+My selection: recent Firefox (Linux & Windows), recent Chrome (Linux & Windows).
+
+__DATA__
+Mozilla/5.0 (X11; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0
+Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36
