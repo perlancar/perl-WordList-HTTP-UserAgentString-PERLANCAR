1ce237870cde599da4d785063a7b8e4c46f9462d
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2019-06-11 11:17:02 +0700

    Update Firefox to 67.0

--- lib/WordList/HTTP/UserAgentString/PERLANCAR.pm
+++ lib/WordList/HTTP/UserAgentString/PERLANCAR.pm
@@ -18,7 +18,7 @@ My selection: recent Firefox (Linux & Windows), recent Chrome (Linux & Windows).
 =cut
 
 __DATA__
-Mozilla/5.0 (Windows NT 6.1; WOW64; rv:66.0) Gecko/20100101 Firefox/66.0
+Mozilla/5.0 (Windows NT 6.1; WOW64; rv:67.0) Gecko/20100101 Firefox/67.0
 Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36
 Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36
-Mozilla/5.0 (X11; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0
+Mozilla/5.0 (X11; Linux x86_64; rv:67.0) Gecko/20100101 Firefox/67.0
