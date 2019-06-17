02200c132b6de2489470f239951bffdbc34baca4
perlancar (on pc-office) <perlancar@gmail.com>
2019-06-17 14:22:30 +0700

    Add newest Chrome (75.0)

--- lib/WordList/HTTP/UserAgentString/PERLANCAR.pm
+++ lib/WordList/HTTP/UserAgentString/PERLANCAR.pm
@@ -20,5 +20,7 @@ My selection: recent Firefox (Linux & Windows), recent Chrome (Linux & Windows).
 __DATA__
 Mozilla/5.0 (Windows NT 6.1; WOW64; rv:67.0) Gecko/20100101 Firefox/67.0
 Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36
+Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36
 Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36
+Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36
 Mozilla/5.0 (X11; Linux x86_64; rv:67.0) Gecko/20100101 Firefox/67.0
